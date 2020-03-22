3f2c6c1be37ad0c16b3d4c221487c2a9ac79780a
AlexeyAndrianov <52441522+KrovYR@users.noreply.github.com>
2020-03-22 18:36:49 +0300

    Completed LW2

---
+++ Lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage remove_duplicates.exe <input string>");
+            }
+            else
+            {
+                Console.WriteLine(args[0].ToCharArray().Distinct().ToArray());
+            }
+        }
+    }
+}
